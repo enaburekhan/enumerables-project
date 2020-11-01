e2bf0f9c5ec474f190086015c6a395a85d72c223
CornerstoneII <seunmyles@gmail.com>
2020-11-01 13:45:13 +0100

    add test case for my_any?

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,15 +4,15 @@ describe Enumerable do
   describe '#my_each' do
     let(:arr) { [1, 2, 3, 4] }
     it 'returns each element on the array' do
-      expect(arr.my_each { |num| num }). to eql([1, 2, 3, 4])    
-    end    
-  end 
-  
+      expect(arr.my_each { |num| num }). to eql([1, 2, 3, 4])
+    end
+  end
+
   describe '#my_each_with_index' do
     let(:arr) { %w[one two three] }
     it 'returns each element with the index value' do
       expect(arr.my_each_with_index { |num, index| "#{num} #{index}" }).to eql(['one 0', 'two 1', 'three 2'])
-    end    
+    end
   end
 
   describe '#my_select' do
@@ -22,8 +22,18 @@ describe Enumerable do
     end
 
     it 'returns an array of odd numbers on the block' do
-      expect(arr.my_select(&:odd?)). to eql([1, 3, 5])    
-    end    
+      expect(arr.my_select(&:odd?)). to eql([1, 3, 5])
+    end
+  end
+
+  describe '#my_all?' do
+    let(:arr) { %w[ant bear cat] }
+    it 'returns true if it matches atleast one condition on the block' do
+      expect(arr.my_any? { |word| word.length >= 3 } ).to eql(true)
+    end
+    it 'returns false if it matches no condition on the block' do
+      expect(arr.my_any?(/d/)).to eql(false)
+    end
   end
 
 end
